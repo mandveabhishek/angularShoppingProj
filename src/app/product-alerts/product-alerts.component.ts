aa0af30c44e52e93a7cf1ca602b5111978bbec59
mandveabhishek <mandveabhishek@gmail.com>
2019-07-20 22:49:19 +0530

    4 files modified, 2 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
+
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
